25c3a8221d11aec8d11ab111df101e8727901085
Gael <demondragong@gmail.com>
2022-05-03 09:56:35 +0200

    correct browser-specific display issues

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,4 @@
+// show or hide navigation menu by changing the classes
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -7,10 +8,12 @@ function editNav() {
   }
 }
 
+// show or hide burger menu
+document.getElementById("burger-menu").addEventListener("click", editNav);
+
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -42,18 +45,19 @@ const conditions = document.getElementById("conditions");
 const form = document.getElementById("signup-form");
 const signupConfirmation = document.getElementById("signup-confirmation");
 
-
+// display an error message
 function showErrorMessage(element, message) {
   element.nextElementSibling.textContent = message;
   element.className = 'text-control invalid';
 }
 
+// clear the error message
 function clearErrorMessage(element) {
   element.nextElementSibling.textContent = '';
   element.className = 'text-control';
 }
 
-// checks firstname field
+// check firstname field
 function checkFirstName() {
   let valid = false;
   if(fname.value.length >= 2) {
@@ -65,7 +69,7 @@ function checkFirstName() {
   return valid;
 }
 
-// checks firstname field
+// check firstname field
 function checkLastName() {
   let valid = false;
   if(lname.value.length >= 2) {
@@ -77,7 +81,7 @@ function checkLastName() {
   return valid;
 }
 
-// checks email field for correct format
+// check email field for correct format
 function checkEmail() {
   let valid = false;
   // email regular expression
@@ -91,7 +95,7 @@ function checkEmail() {
   return valid;
 }
 
-// checks a birthdate has been provided
+// check a birthdate has been provided
 function checkBirthdate() {
   let valid = false;
   if (birthdate.value) {
@@ -103,7 +107,7 @@ function checkBirthdate() {
   return valid;
 }
 
-// checks a number of tournaments has been provided
+// check a number of tournaments has been provided
 function checkNumberOfTournaments() {
   let valid = false;
   if (numberOfTournaments.value) {
@@ -115,7 +119,7 @@ function checkNumberOfTournaments() {
   return valid;
 }
 
-// checks a location has been selected
+// check a location has been selected
 function checkLocation() {
   let valid = false;
   if (document.querySelector('input[name="location"]:checked')) {
@@ -127,7 +131,7 @@ function checkLocation() {
   return valid;
 }
 
-// checks the conditions have been accepted
+// check the conditions have been accepted
 function checkConditions() {
   let valid = false;
   if (document.getElementById("checkbox1").checked) {
@@ -139,13 +143,27 @@ function checkConditions() {
   return valid;
 }
 
+// delay the execution of the validation functions to improve form perfomance and prevent from sending error messages immediately to the user
+const debounce = (fn, delay = 500) => {
+  let timeoutId;
+  return (...args) => {
+      // cancel the previous timer
+      if (timeoutId) {
+          clearTimeout(timeoutId);
+      }
+      // setup a new timer
+      timeoutId = setTimeout(() => {
+          fn.apply(null, args)
+      }, delay);
+  };
+};
 
 // event listeners on individual form fields
-fname.addEventListener('input', checkFirstName);
-lname.addEventListener('input', checkLastName);
-email.addEventListener('input', checkEmail);
-birthdate.addEventListener('input', checkBirthdate);
-numberOfTournaments.addEventListener('input', checkNumberOfTournaments);
+fname.addEventListener('input', debounce(checkFirstName));
+lname.addEventListener('input', debounce(checkLastName));
+email.addEventListener('input', debounce(checkEmail));
+birthdate.addEventListener('input', debounce(checkBirthdate));
+numberOfTournaments.addEventListener('input', debounce(checkNumberOfTournaments));
 locationContainer.addEventListener('input', checkLocation);
 document.getElementById("checkbox1").addEventListener('input', checkConditions);  
 
@@ -173,9 +191,21 @@ form.addEventListener('submit', function (e) {
       isLocationValid &&
       isConditionsValid;
 
-  // submit to the server if the form is valid. For this exercise: we'll hide the signup form and show a confirmation message
+  // submit to the server if the form is valid. For this exercise: we'll simply clear the form fields, hide the signup form and show a confirmation message
   if (isFormValid) {
+    // clear form fields
+    fname.value = "";
+    lname.value = "";
+    email.value = "";
+    birthdate.value = "";
+    numberOfTournaments.value = "";
+    document.querySelector('input[name="location"]:checked').checked = false;
+    document.getElementById("checkbox2").checked = false;
+    
+    // hide signup form
     form.style.display = "none";
+    
+    // display confirmation message
     signupConfirmation.style.display = "block";
   }
 });
